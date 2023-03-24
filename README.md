164f576cfca0647fff54a58517ceae72fcba52a5
Dee Baptiste <darren@darrenbaptiste.com>
2023-03-24 15:06:52 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ShoppingListDataa
+
+A description of this package.
---
+++ Sources/ShoppingListDataa/ShoppingListDataa.swift
@@ -0,0 +1,6 @@
+public struct ShoppingListDataa {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
